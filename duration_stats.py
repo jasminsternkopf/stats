b4e42d5d99a0e194f93f535fea3bf6e30d8f3540
Jasmin Sternkopf <jasmin_sternkopf@web.de>
2021-02-17 15:01:54 +0100

    small corrections

--- duration_stats.py
+++ duration_stats.py
@@ -5,7 +5,7 @@ import numpy as np
 
 from typing import TypeVar, Dict, List
 
-
+FIRST_COL = "SPEAKER"
 _T = TypeVar('_T')
 
 def get_duration_stats(speakers: List[str], data_trn: Dict[str, List[_T]], data_val: Dict[str, List[_T]], data_tst: Dict[str, List[_T]], data_rst: Dict[str, List[_T]], data_total: Dict[str, List[_T]]) -> pd.DataFrame:
@@ -18,11 +18,11 @@ def get_duration_stats(speakers: List[str], data_trn: Dict[str, List[_T]], data_
   mean_df = get_mean_df(speakers, meta_dataset)
   full_df = pd.concat([
       duration_df,
-      rel_duration_df.loc[:, rel_duration_df.columns != "SPEAKER"],
-      dist_df.loc[:, dist_df.columns != "SPEAKER"],
-      min_df.loc[:, min_df.columns != "SPEAKER"],
-      max_df.loc[:, max_df.columns != "SPEAKER"],
-      mean_df.loc[:, mean_df.columns != "SPEAKER"]
+      rel_duration_df.loc[:, rel_duration_df.columns != FIRST_COL],
+      dist_df.loc[:, dist_df.columns != FIRST_COL],
+      min_df.loc[:, min_df.columns != FIRST_COL],
+      max_df.loc[:, max_df.columns != FIRST_COL],
+      mean_df.loc[:, mean_df.columns != FIRST_COL]
       ],
       axis=1,
       join='inner')
@@ -101,14 +101,18 @@ def get_minimum_durations_for_one_speaker_for_all_sets(speaker,durations_list: L
   return mins
 
 def get_dist_df(durations_df: pd.DataFrame) -> pd.DataFrame:
-  df = durations_df.loc[0:len(durations_df.index)-2, durations_df.columns != "SPEAKER"].copy()
+  durations_df.replace("-",0,inplace=True)
+  df = durations_df.loc[0:len(durations_df.index)-2, durations_df.columns != FIRST_COL].copy()
   df.rename(columns = {"DUR TRN": "DIST TRN"}, inplace = True)
   dataset_lengths = df.sum()
   df = 100* df.div(dataset_lengths)
-  df.insert(loc=0, column="SPEAKER", value = durations_df.loc[0:len(durations_df.index)-2, durations_df.columns=="SPEAKER"])
+  df.insert(loc=0, column=FIRST_COL, value = durations_df.loc[0:len(durations_df.index)-2, durations_df.columns==FIRST_COL])
   last_line = df.sum()
   df=df.append(last_line, ignore_index=True)
   df.iloc[-1,0] ="all"
+  df.replace(0,"-",inplace=True)
+  df.replace(NaN,"-",inplace=True)
+  durations_df.replace(0,"-",inplace=True)
   return df
 
 def get_whole_dataset_duration(dataset: Dict[str, List[List[_T]]]) -> _T:
@@ -116,9 +120,10 @@ def get_whole_dataset_duration(dataset: Dict[str, List[List[_T]]]) -> _T:
   return sum(duration_for_each_speaker)
 
 def get_rel_duration_df(durations_df: pd.DataFrame) -> pd.DataFrame:
+  durations_df.replace("-",0,inplace=True)
   df_as_row_wise_array = durations_df.to_numpy()
   df_lines = []
-  for row in df_as_row_wise_array:
+  for row in df_as_row_wise_array[:-1,:]:
       rel_durations_list = get_relative_durations_for_all_sets(row[1:])
       rel_durations_list.insert(0, row[0])
       df_lines.append(rel_durations_list)
@@ -126,12 +131,15 @@ def get_rel_duration_df(durations_df: pd.DataFrame) -> pd.DataFrame:
   last_line = df.sum()
   df=df.append(last_line, ignore_index=True)
   df.iloc[-1,0] ="all"
-  df.iloc[-1, 1:] = df.iloc[-1, 1:].div(4)
+  norm_factor = last_line[1:].sum()/100
+  df.iloc[-1, 1:] = df.iloc[-1, 1:].div(norm_factor)
+  df.replace(0,"-",inplace=True)
+  durations_df.replace(0,"-",inplace=True)
   return df
 
 def get_relative_durations_for_all_sets(duration_list: List[_T]) -> List:
   if duration_list[-1] == 0: #falls ein Sprecher in keinem Set vorkommt
-    return [0]*(len(duration_list)-1) # eigentlich '-'
+    return [0]*(len(duration_list)-1)
   rel_durations=100*np.array(duration_list[:-1])/duration_list[-1]
   return rel_durations.tolist()
 
@@ -142,6 +150,7 @@ def get_duration_df(speakers: List[str], meta_dataset: Dict[str, List[List[_T]]]
   last_line = df.sum()
   df=df.append(last_line, ignore_index=True)
   df.iloc[-1,0] ="all"
+  df.replace(0,"-",inplace=True)
   return df
 
 def get_duration_sums_for_every_speaker_for_all_sets(speakers: List[str], dataset: Dict[str, List[List[_T]]]) -> List[List]:
--- duration_stats.tests.py
+++ duration_stats.tests.py
@@ -5,13 +5,15 @@ import pandas as pd
 
 TRAIN = {"a": [1,2,3], "b": [3,4], "d": [1,3,4]}
 VAL = {"a": [1,1], "b": [2,1], "c": [1]}
-TEST = {"b":[3,5], "c": [7]}
-REST = {"d": [4,5]}
-TOTAL={"a": [1,2,3,1,1], "b": [3,4,2,1,3,5], "c":[1,7], "d": [1,3,4,4,5]}
-SPEAKERS = ["a","c","b","d","e"]
+TEST = {"b":[3,5], "c": [7], "f": [1,3]}
+REST = {}
+#REST = {"d": [4,5]}
+#TOTAL={"a": [1,2,3,1,1], "b": [3,4,2,1,3,5], "c":[1,7], "d": [1,3,4,4,5]}
+TOTAL={"a": [1,2,3,1,1], "b": [3,4,2,1,3,5], "c":[1,7], "d": [1,3,4], "f": [1,3]}
+SPEAKERS = ["a","c","b","d","e","f"]
 
 meta = get_meta_dict(SPEAKERS, TRAIN, VAL, TEST, REST,TOTAL)
-print(meta)
+#print(meta)
 
 full=get_duration_stats(SPEAKERS, TRAIN, VAL, TEST, REST, TOTAL)
 print(full.head(10))
@@ -27,8 +29,8 @@ df = get_duration_df(SPEAKERS, meta)
 #print(df.sum())#["VAL","TST","RST"]
 #dist = get_dist_df(df)
 #print(dist.head())
-#reldf = get_rel_duration_df(df)
-#print(reldf.head())
+reldf = get_rel_duration_df(df)
+#print(reldf.head(10))
 # mindf=get_min_df(meta)
 # print(mindf.head())
 # maxdf=get_max_df(meta)
